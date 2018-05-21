bf018880de7d6e38a52bc84ad21574b965010acc
RRoggia <renanzr@gmail.com>
2018-05-21 20:02:19 -0300

    add first test

---
+++ src/test/java/com/sap/rroggia/CalculatorTest.java
@@ -0,0 +1,20 @@
+package com.sap.rroggia;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void shouldAddTwoValues() {
+		int firstValue = 10;
+		int secondValue = 20;
+		int expectedValue = 30;
+
+		int returningValue = new Calculator().add(firstValue, secondValue);
+
+		assertEquals("The returning value should be the sum of both values", expectedValue, returningValue);
+	}
+
+}
