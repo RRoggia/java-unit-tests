66d2f48f8ecd7b2ef49b8bcbcfa93ed931543305
RRoggia <renanzr@gmail.com>
2018-05-21 19:53:35 -0300

    add calculator class and its four basic methods

---
+++ src/main/java/com/sap/rroggia/Calculator.java
@@ -0,0 +1,21 @@
+package com.sap.rroggia;
+
+public class Calculator {
+
+	public int add(int firstValue, int secondValue) {
+		return firstValue + secondValue;
+	}
+
+	public int multiply(int firstValue, int secondValue) {
+		return firstValue * secondValue;
+	}
+
+	public int divide(int firstValue, int secondValue) {
+		return firstValue / secondValue;
+	}
+
+	public int minus(int firstValue, int secondValue) {
+		return firstValue - secondValue;
+	}
+
+}
