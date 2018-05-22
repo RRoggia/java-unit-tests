6f18600af7ba255df34cd0e6c24c791602e0dbd8
RRoggia <renanzr@gmail.com>
2018-05-22 13:38:24 -0300

    showing matchers by testing a singleton

--- src/test/java/com/sap/rroggia/CalculatorTest.java
+++ src/test/java/com/sap/rroggia/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.sap.rroggia;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,13 +13,11 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	private static Calculator calculator;
-	private static final int firstValue = 10;
-	private static final int secondValue = 20;
 
 	@BeforeClass
 	public static void setUpClass() {
 		System.out.println("Executed once before tests, when initializing the class.");
-		calculator = new Calculator();
+		calculator = Calculator.getInstance();
 	}
 
 	@Before
@@ -27,20 +27,17 @@ public class CalculatorTest {
 
 	@Test
 	public void shouldAddTwoValues() {
-		int expectedValue = 30;
-
-		int returningValue = calculator.add(firstValue, secondValue);
-
-		assertEquals("The returning value should be the sum of both values", expectedValue, returningValue);
+		assertThat(calculator.add(10, 20), is(30));
 	}
 
 	@Test
 	public void shouldMultiplyTwoValues() {
-		int expectedValue = 200;
+		assertThat(calculator.multiply(10, 20), is(200));
+	}
 
-		int returningValue = calculator.multiply(firstValue, secondValue);
-
-		assertEquals(expectedValue, returningValue);
+	@Test
+	public void shouldReturnTheSameInstance() {
+		assertThat(Calculator.getInstance(), sameInstance(calculator));
 	}
 
 	@After
